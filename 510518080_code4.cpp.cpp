1e88c8cf0a99e7b83b7e9f1b4062f838833a84fd
amor <amoriqbalmail@gmail.com>
2020-10-20 15:10:45 +0530

    translation

--- 510518080_code4.cpp.cpp
+++ 510518080_code4.cpp.cpp
@@ -4,35 +4,37 @@
 #include <chrono>
 #include <thread>
 #include <unistd.h>
+#include <math.h>
+
+
 class MovingEllipse : public MidEllipse{
 public:
     volatile int state;
-    int cx,cy;
-    MovingEllipse():cx(-8),cy(0),state(0){}
-    void setCenter(int cx1,int cy1){
-        cx=cx1;
-        cy=cy1;
+    float th;
+    MovingEllipse():th(0),state(1){}
+    void setTheta(float t){
+        th=t;
         this->update();
     }
     void virtual paint(QPainter *g){
         if(state==1){
-            drawBird(g,cx,cy);
-            setCenter(cx-1,cy);
-            this->update();
+            drawSettings(g,th);
+            setTheta(th+0.1);
         }
         else{
-            drawBird(g,cx,cy);
+        drawSettings(g,th);
         }
+        //drawBird(g,-40,0);
     }
     void drawBird(QPainter *g, int cx, int cy){
-        drawTail(g,65+cx,-8+cy,Qt::red,Qt::red);
+        drawTail(g,65+cx,-8+cy,Qt::black,Qt::magenta);
         drawEllipse(g,18+cx,0+cy,8,8,Qt::black,Qt::yellow,true,true,true,true);
         drawEllipse(g,45+cx,-11+cy,24,12,Qt::black,Qt::yellow,true,true,true,true);
         drawEllipse(g,45+cx,-2+cy,14,14,Qt::black,Qt::magenta,true,true,true,true);
         drawEllipse(g,45+cx,-11+cy,24,12,Qt::black,Qt::yellow,false,false,true,true);
-        //midpoint(g,35+cx,-10+cy,55+cx,-10+cy,Qt::black);
-        drawEllipse(g,18+cx,3+cy,2,2,Qt::blue,Qt::cyan,true,true,true,true);
-        drawPeck(g,0+cx,0+cy,Qt::red,Qt::red);
+        midpoint(g,35+cx,-10+cy,55+cx,-10+cy,Qt::black);
+        drawEllipse(g,18+cx,3+cy,2,2,Qt::black,Qt::cyan,true,true,true,true);
+        drawPeck(g,0+cx,0+cy,Qt::black,Qt::red);
     }
     void drawPeck(QPainter *g, int cx, int cy, QColor bound, QColor fill,int scale=1){
         int xarr[]={cx,cx+10,cx+10};
@@ -48,7 +50,27 @@ public:
         boundaryFill(g,cx+2,cy+1,fill);
     }
 
+    void drawSettings(QPainter *g, float theta){
+        int n=8;
+        int r1=10,r2=15,r3=20;
+        int s=3.14*r2/n*0.7;
+        std::vector<int> ptsx,ptsy;
+        for(int i=0;i<n;i++){
+            float angle=2*3.142/n*i+theta;
+            ptsx.push_back(s*std::cos(angle)-r2*std::sin(angle));
+            ptsy.push_back(s*std::sin(angle)+r2*std::cos(angle));
+            ptsx.push_back(s*std::cos(angle)-r3*std::sin(angle));
+            ptsy.push_back(s*std::sin(angle)+r3*std::cos(angle));
+            ptsx.push_back(-s*std::cos(angle)-r3*std::sin(angle));
+            ptsy.push_back(-s*std::sin(angle)+r3*std::cos(angle));
+            ptsx.push_back(-s*std::cos(angle)-r2*std::sin(angle));
+            ptsy.push_back(-s*std::sin(angle)+r2*std::cos(angle));
+}
 
+        drawPoly(g,4*n,ptsx.data(),ptsy.data(),Qt::black);
+        drawEllipse(g,0,0,r1,r1,Qt::black,Qt::white,true,true,true,true);
+        boundaryFill(g,0,r1+1,Qt::darkGray);
+    }
 private slots:
     void virtual on_pushButton_clicked(){
         if(state==0){
